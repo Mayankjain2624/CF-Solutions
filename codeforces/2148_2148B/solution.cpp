e8afaf1edf907bb2dd9664481b7b728e861552eb
MAYANK JAIN <162597810+Mayankjain2624@users.noreply.github.com>
2025-09-26 02:18:27 +0530

    Add solution for 2148_2148B

---
+++ codeforces/2148_2148B/solution.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m,x,y;
+        cin>>n>>m>>x>>y;
+        int k=0;
+        for(int i=0;i<n;i++)
+        cin>>k;
+        for(int i=0;i<m;i++)
+        cin>>k;
+        cout<<n+m<<endl;
+    }
+}
